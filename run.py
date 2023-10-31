72aebd1e936d58bd9599305979304fe73577cffd
Izzo2le <>
2023-10-31 18:39:30 +0000

    ADD BATTLESHIPS GAME

--- run.py
+++ run.py
@@ -1,3 +1,66 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+def print_board(board):
+    print(' A B C D E F G H')
+    print(' ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+def place_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0,7), randint(0,7)
+        board[ship_row][ship_column] = 'X'
+
+def get_ship_location():
+    row = input('Please enter a ship row, using numbers 1-8: ')
+    while row not in '12345678':
+        print('please enter a valid row')
+        row = input('Please enter a ship row, using numbers 1-8: ')
+    column = input('Please enter a ship column, using letters A-H: ').upper()
+    while column not in 'ABCDEFGH':
+        print('Please enter a valid column')
+        column = input('Please enter a ship column A-H').upper()
+    return int(row) - 1, letters_to_numbers[column]
+
+def count_hit_ships(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count
+
+def play_game():
+    place_ships(HIDDEN_BOARD)
+    turns = 10
+    while turns > 0:
+        print('Hi, Welcome to my battleships game')
+        print_board(GUESS_BOARD)
+        row, column = get_ship_location()
+        if GUESS_BOARD[row][column] == '-':
+            print('You have already guessed that')
+        elif HIDDEN_BOARD[row][column] == 'X':
+            print('Yay congratulations, you have hit a battleship')
+            GUESS_BOARD[row][column] = 'X'
+            turns -= 1
+        else:
+            print('Oh no sorry, you missed')
+            GUESS_BOARD[row][column] = '-'
+            turns -= 1
+        if count_hit_ships(GUESS_BOARD) == 5:
+            print('Yay congratulations, you have sunk all the battleships')
+            break
+        print('you have ' + str(turns) + ' turns remaining')
+        if turns == 0:
+            print('Sorry, your game is over')
+            break
+
+play_game()
